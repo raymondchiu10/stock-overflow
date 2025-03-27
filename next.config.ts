9a46dae3db392010ba7be8cf3851348392395b8a
Raymond Chiu <raymondchiu10@gmail.com>
2025-03-27 12:06:03 -0700

    add backend routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*", // Proxy all requests from /api/* to your backend
+				destination: `${process.env.NEXT_BACKEND_URL}/:path*`, // Change this to your backend URL
+			},
+		];
+	},
 };
 
 export default nextConfig;
